27d45535b46dd90c5c5e8d9423632d417066e170
H0gni Gylfsaon <hognig@vis.is>
2012-09-04 20:49:18 +0000

    Split and confgured some of the settings

---
+++ knittygritty/settings/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Django settings compiler
+"""
+from __future__ import absolute_import
+
+
+from . common import *
+from . staticfiles import *
--- knittygritty/settings/common.py
+++ knittygritty/settings/common.py
@@ -1,10 +1,24 @@
-# Django settings for knittygritty project.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Django settings compiler
+"""
+from __future__ import absolute_import
+
+import os
+
+ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..')
+)
+
+print ROOT
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Högni Gylfason', 'klumhru@gmail.com'),
+    ('Amy Tucker', 'zamalynn@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -42,40 +56,6 @@ USE_L10N = True
 
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '))h(71e0@%*0kzh#i=u%b0rbt%ah4w*sr%nvg(_40xazkds3l('
